ce16bf59786330110434d51bca34aaa0a8d2c2ec
worgflores <worgflores@gmail.com>
2013-08-12 18:01:59 +0800

    updated the rameses-enterprise-admin-services

--- workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/usergroup.sql
+++ workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/usergroup.sql
@@ -1,30 +1,39 @@
 [getRootNodes]
-SELECT DISTINCT ug.domain as caption, ug.domain as domain, '' as usergroupid, 'domain' as filetype
-FROM sys_usergroup ug
-LEFT JOIN sys_usergroup_admin uga ON ug.objid=uga.usergroupid
-WHERE (uga.user_objid =  $P{userid} OR 'root' = $P{userid} OR 'sa' = $P{userid})
+SELECT DISTINCT 
+	ug.domain as caption, ug.domain as domain, '' as usergroupid, 'domain' as filetype 
+FROM sys_usergroup ug 
+	LEFT JOIN sys_usergroup_admin uga ON ug.objid=uga.usergroupid 
+WHERE 
+	(uga.user_objid=$P{userid} OR 'root'=$P{userid} OR 'sa'=$P{userid}) 
+
 
 [getChildNodes]
-SELECT ug.title as caption, ug.domain as domain, ug.objid as usergroupid, 'usergroup-folder' as filetype, ug.orgclass
-FROM sys_usergroup ug
-LEFT JOIN sys_usergroup_admin uga ON ug.objid=uga.usergroupid
-WHERE ( uga.user_objid = $P{userid} OR 'root' = $P{userid} OR 'sa' = $P{userid} )
-AND ug.domain=$P{domain}
-
+SELECT DISTINCT 
+	ug.title as caption, ug.domain as domain, ug.objid as usergroupid, 
+	'usergroup-folder' as filetype, ug.orgclass 
+FROM sys_usergroup ug 
+	LEFT JOIN sys_usergroup_admin uga ON ug.objid=uga.usergroupid 
+WHERE 
+	ug.domain=$P{domain} AND 
+	(uga.user_objid=$P{userid} OR 'root'=$P{userid} OR 'sa'=$P{userid}) 
 
 [getList]
-SELECT ugm.objid, ugm.user_username, ugm.user_lastname, ugm.user_firstname, sg.name AS securitygroup, ugm.org_name
-FROM sys_usergroup_member ugm
-INNER JOIN sys_securitygroup sg ON ugm.securitygroupid=sg.objid 
-LEFT JOIN sys_usergroup_admin uga ON ugm.usergroupid=uga.usergroupid
-WHERE ugm.usergroupid=$P{usergroupid}
-AND ( uga.user_objid = $P{userid} OR 'root' = $P{userid} OR 'sa' = $P{userid} )
-
+SELECT DISTINCT 
+	ugm.objid, ugm.user_username, ugm.user_lastname, ugm.user_firstname, 
+	ugm.org_name, sg.name AS securitygroup 
+FROM sys_usergroup ug 
+	INNER JOIN sys_usergroup_member ugm ON ug.objid=ugm.usergroupid ${usergroupfilter} 
+	INNER JOIN sys_securitygroup sg ON ugm.securitygroupid=sg.objid 
+	LEFT JOIN sys_usergroup_admin uga ON ugm.usergroupid=uga.usergroupid 
+WHERE 
+	ug.domain=$P{domain} AND 
+	(uga.user_objid=$P{userid} OR 'root'=$P{userid} OR 'sa'=$P{userid}) 	
+ORDER BY 
+	ugm.user_lastname, ugm.user_firstname 
 
 [getAdminList]
 SELECT uga.* FROM sys_usergroup_admin uga
 WHERE uga.usergroupid=$P{usergroupid}
-
 
 [search]
 SELECT ugm.objid, su.username, su.name, sg.name AS securitygroup, so.name as org
