3429b1b69edc913c2777ed3f5b6c34be18975ec6
worgflores <worgflores@gmail.com>
2013-08-13 15:41:00 +0800

    updated the rameses-gov-lgu

--- workspace/services/shared/v25/modules/rameses-gov-lgu-services.jar/sql/city.sql
+++ workspace/services/shared/v25/modules/rameses-gov-lgu-services.jar/sql/city.sql
@@ -3,3 +3,10 @@ SELECT b.objid, b.state, b.indexno, b.pin, b.name
 FROM city b 
 WHERE b.name LIKE $P{name}  
 ORDER BY b.name 
+
+[changeState]
+UPDATE city SET 
+	state=$P{newstate} 
+WHERE 
+	objid=$P{objid} AND state=$P{oldstate} 
+
--- workspace/services/shared/v25/modules/rameses-gov-lgu-services.jar/sql/province.sql
+++ workspace/services/shared/v25/modules/rameses-gov-lgu-services.jar/sql/province.sql
@@ -3,3 +3,10 @@ SELECT b.objid, b.state, b.indexno, b.pin, b.name
 FROM province b 
 WHERE b.name LIKE $P{name}  
 ORDER BY b.name 
+
+[changeState]
+UPDATE province SET 
+	state=$P{newstate} 
+WHERE 
+	objid=$P{objid} AND state=$P{oldstate} 
+
