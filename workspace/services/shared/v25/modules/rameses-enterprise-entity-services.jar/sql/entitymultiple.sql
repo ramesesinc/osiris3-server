61e5f7c1434343a39ebf15ea423e43cc78b84d97
worgflores <worgflores@gmail.com>
2013-08-15 13:06:21 +0800

    added new shared services rameses-enterprise-entity-services

---
+++ workspace/services/shared/v25/modules/rameses-enterprise-entity-services.jar/sql/entitymultiple.sql
@@ -0,0 +1,14 @@
+[getList]
+SELECT DISTINCT e.*, emu.fullname 
+FROM entitymember em 
+	INNER JOIN entitymultiple emu ON em.entityid=emu.objid 
+	INNER JOIN entity e ON emu.objid=e.objid 
+WHERE em.taxpayer_name LIKE $P{name} 
+ORDER BY em.taxpayer_name 
+
+
+[getMembers]
+SELECT * FROM entitymember WHERE entityid=$P{objid} ORDER BY itemno 
+
+[removeMembers]
+DELETE FROM entitymember WHERE entityid=$P{objid} 
