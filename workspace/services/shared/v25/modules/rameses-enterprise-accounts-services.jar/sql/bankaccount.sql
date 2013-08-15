76775c1fe1ac1ff89a761cf974336f5d60d0cbfa
elmo <elmonazareno@gmail.com>
2013-08-15 12:00:55 +0800

    bank service

--- workspace/services/shared/v25/modules/rameses-enterprise-accounts-services.jar/sql/bankaccount.sql
+++ workspace/services/shared/v25/modules/rameses-enterprise-accounts-services.jar/sql/bankaccount.sql
@@ -1,5 +1,7 @@
 [getList]
-SELECT * FROM bankaccount 
+SELECT ba.*, b.branchname 
+FROM bankaccount ba 
+INNER JOIN bank b ON ba.bank_objid=b.objid
 
 [changeState-approved]
 UPDATE bankaccount SET state=$P{newstate} WHERE objid=$P{objid} AND NOT(state='APPROVED')
