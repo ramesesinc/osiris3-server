5f1bb9e88792c63ddcfed464dd059e4091b1bfea
elmo <elmonazareno@gmail.com>
2013-08-16 22:32:25 +0800

    updated the rameses-enterprise-treasury-services

--- workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/cashbook.sql
+++ workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/cashbook.sql
@@ -1,11 +1,14 @@
 [getList]
 SELECT * FROM cashbook 
 
-[getEntries]
-SELECT * FROM cashbook_entry WHERE parent=$P{objid}
-
 [changeState-approved]
 UPDATE cashbook SET state='APPROVED' WHERE objid=$P{objid}
 
 [findByCollectorFund]
 SELECT * FROM cashbook WHERE fund_objid=$P{fundid} AND subacct_objid=$P{subacctid}
+
+[getEntries]
+SELECT refdate,refno,reftype,particulars,dr,cr,runbalance,pageno,lineno 
+FROM cashbook_entry 
+WHERE parentid=$P{objid} 
+order by pageno, lineno 
--- workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/cashreceipt.sql
+++ workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/cashreceipt.sql
@@ -2,12 +2,18 @@
 SELECT * FROM cashreceipt_txntype 
 
 [getList]
-SELECT c.*, CASE WHEN r.receiptid IS NULL THEN 0 ELSE 1 END AS remitted  
+SELECT c.*, 
+CASE WHEN v.receiptid IS NULL THEN 0 ELSE 1 END AS voided,  
+CASE WHEN r.receiptid IS NULL THEN 0 ELSE 1 END AS remitted  
 FROM cashreceipt c 
 LEFT JOIN remittanceitem r ON c.objid=r.receiptid
+LEFT JOIN cashreceipt_void v ON c.objid=v.receiptid
 
 [getItems]
-SELECT * FROM cashreceiptitem WHERE receiptid=$P{objid}
+SELECT ci.*, r.fund_objid AS item_fund_objid, r.fund_title AS item_fund_title
+FROM cashreceiptitem ci
+INNER JOIN revenueitem r ON r.objid = ci.item_objid
+WHERE ci.receiptid=$P{objid}
 
 [getCheckPayments]
 SELECT * FROM cashreceiptpayment_check WHERE receiptid=$P{objid}
