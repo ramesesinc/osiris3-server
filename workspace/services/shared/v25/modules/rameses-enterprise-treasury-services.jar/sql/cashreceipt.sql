4e0eb4c772cde607c11dc00e901647e8a42f19a1
elmo <elmonazareno@gmail.com>
2013-08-16 19:39:37 +0800

    added receipt query

--- workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/cashreceipt.sql
+++ workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/cashreceipt.sql
@@ -1,2 +1,13 @@
 [getTxnTypes]
 SELECT * FROM cashreceipt_txntype 
+
+[getList]
+SELECT c.*, CASE WHEN r.receiptid IS NULL THEN 0 ELSE 1 END AS remitted  
+FROM cashreceipt c 
+LEFT JOIN remittanceitem r ON c.objid=r.receiptid
+
+[getItems]
+SELECT * FROM cashreceiptitem WHERE receiptid=$P{objid}
+
+[getCheckPayments]
+SELECT * FROM cashreceiptpayment_check WHERE receiptid=$P{objid}
