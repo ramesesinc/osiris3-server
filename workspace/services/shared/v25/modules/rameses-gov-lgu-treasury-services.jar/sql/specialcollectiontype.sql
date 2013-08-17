a088b4ed29a8c616ce13fb58b4750f1c2875ea04
Jessie S. Zamora <jzamss@gmail.com>
2013-08-17 10:31:29 +0800

    add rameses-gov-lgu-treasury-services.jar

---
+++ workspace/services/shared/v25/modules/rameses-gov-lgu-treasury-services.jar/sql/specialcollectiontype.sql
@@ -0,0 +1,24 @@
+[create]
+INSERT INTO specialaccountsetting 
+	(objid, item_objid, amount)
+VALUES
+	($P{objid}, $P{item_objid}, $P{amount})
+
+[update]	
+UPDATE specialaccountsetting SET
+	item_objid = $P{item_objid},
+	amount = $P{amount}
+WHERE objid = $P{objid}
+
+[read]
+SELECT 
+	ss.*,
+	ri.title AS item_title,
+	ri.code AS item_code,
+	ri.fund_objid AS item_fund_objid
+FROM specialaccountsetting ss 
+	INNER JOIN revenueitem ri ON ss.item_objid = ri.objid 
+WHERE ss.objid = $P{objid}	
+
+[delete]
+DELETE FROM specialaccountsetting WHERE objid = $P{objid}
