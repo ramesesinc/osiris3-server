88e9bd5f078a237b8f90db9cb972b208cb140575
Jessie S. Zamora <jzamss@gmail.com>
2013-08-17 14:08:16 +0800

    Add search to Bank Account

--- workspace/services/shared/v25/modules/rameses-enterprise-accounts-services.jar/sql/bankaccount.sql
+++ workspace/services/shared/v25/modules/rameses-enterprise-accounts-services.jar/sql/bankaccount.sql
@@ -2,6 +2,10 @@
 SELECT ba.*, b.branchname, ba.fund_code, ba.fund_title 
 FROM bankaccount ba 
 INNER JOIN bank b ON ba.bank_objid=b.objid
+WHERE ba.code LIKE $P{searchtext}
+   OR ba.bank_code LIKE $P{searchtext}
+   OR ba.bank_name LIKE $P{searchtext}
+ORDER BY ba.code
 
 [changeState-approved]
 UPDATE bankaccount SET state=$P{newstate} WHERE objid=$P{objid} 
