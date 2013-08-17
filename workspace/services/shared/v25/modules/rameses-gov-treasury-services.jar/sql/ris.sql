7516c0342c38e31dc8e9165495fb8a943b71a4ce
Arnel Retiza <retizaarnel@yahoo.com.ph>
2013-08-17 17:55:15 +0800

    RIS/RIV report service

--- workspace/services/shared/v25/modules/rameses-gov-treasury-services.jar/sql/ris.sql
+++ workspace/services/shared/v25/modules/rameses-gov-treasury-services.jar/sql/ris.sql
@@ -7,3 +7,28 @@ SELECT * FROM risitem WHERE parentid=$P{objid}
 
 [changeState-approved]
 UPDATE ris SET state='APPROVED' WHERE objid=$P{objid}
+
+[getRISInfo]
+select 
+    risno, txntype, dtfiled, requester_name, 
+    requester_title, approver_name, approver_title 
+from ris     
+where objid=$P{risid}
+
+[getRISAFIssueItem]
+select 
+	ri.item_objid, ri.unit, ri.qty as qtyrequested, 
+	afi.endseries, afi.startseries, afi.startstub, afi.endstub, afi.qty as qtyreceived 
+from risitem ri 
+  left join afissue af on ri.parentid = af.risid 
+  left join afissueitem afi on afi.parentid = af.objid 
+where ri.parentid=$P{risid}
+
+[getRISReceiptItem]
+select 
+	ri.item_objid, ri.unit, ri.qty as qtyrequested, 
+	afi.endseries, afi.startseries, afi.startstub, afi.endstub, afi.qty as qtyreceived 
+from risitem ri 
+  left join afreceipt af on ri.parentid = af.risid 
+  left join afreceiptitem afi on afi.parentid = af.objid 
+where ri.parentid=$P{risid}
