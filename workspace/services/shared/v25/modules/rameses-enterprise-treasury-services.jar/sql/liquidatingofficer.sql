c82456230b3fb323609e01099766fa1ef3547ff6
elmo <elmonazareno@gmail.com>
2013-08-18 23:48:17 +0800

    added liquidation

--- workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/liquidatingofficer.sql
+++ workspace/services/shared/v25/modules/rameses-enterprise-treasury-services.jar/sql/liquidatingofficer.sql
@@ -6,3 +6,10 @@ user_lastname as lastname,
 user_firstname as firstname,
 jobtitle as title
 FROM sys_usergroup_member WHERE usergroupid = 'LIQUIDATING_OFFICER'
+
+
+[getUserTxnCode]
+SELECT ug.usertxncode 
+FROM sys_usergroup_member ug 
+WHERE ug.user_objid = $P{userid}
+AND usergroupid = 'LIQUIDATING_OFFICER'
