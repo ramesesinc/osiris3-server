1b6baa1c4660e18ba986cd6649a56964f8a11bc7
worgflores <worgflores@gmail.com>
2013-08-22 10:30:34 +0800

    updated the UsergroupMemberLookupService and usergroupmember.sql by providing a find facility

--- workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/usergroupmember.sql
+++ workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/usergroupmember.sql
@@ -6,3 +6,7 @@ FROM sys_usergroup_member ugm
 	INNER JOIN sys_user u ON ugm.user_objid=u.objid 
 WHERE ugm.usergroupid=$P{usergroupid} ${filter} 
 ORDER BY ugm.user_lastname 
+
+[find]
+SELECT * FROM sys_usergroup_member 
+WHERE usergroupid=$P{usergroupid} and user_objid=$P{userid}  
