3fd3833a177bab8c566f2771700d434c33be3654
worgflores <worgflores@gmail.com>
2013-08-23 10:49:47 +0800

    updated the usergroupmember.sql

--- workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/usergroupmember.sql
+++ workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/usergroupmember.sql
@@ -1,7 +1,7 @@
 [lookup]
 SELECT 
-	u.lastname, u.firstname, u.middlename, u.jobtitle, 
-	u.objid AS userid, ugm.objid, ugm.usergroupid  
+	u.objid, u.lastname, u.firstname, u.middlename, u.jobtitle, 
+	ugm.objid as usergroupmemberid, ugm.usergroupid  
 FROM sys_usergroup_member ugm 
 	INNER JOIN sys_user u ON ugm.user_objid=u.objid 
 WHERE ugm.usergroupid=$P{usergroupid} ${filter} 
