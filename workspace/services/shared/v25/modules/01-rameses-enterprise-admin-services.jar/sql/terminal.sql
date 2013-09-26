7a1657f972eccded5d76d2269a55271a6e105ea1
worgflores <worgflores@gmail.com>
2013-09-26 18:52:39 +0800

    updated the terminal.sql

--- workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/terminal.sql
+++ workspace/services/shared/v25/modules/01-rameses-enterprise-admin-services.jar/sql/terminal.sql
@@ -1,6 +1,18 @@
 [getList]
 SELECT * FROM sys_terminal 
 
+[findTerminal]
+select * from sys_terminal where terminalid=$P{terminalid} 
+
+[findMacAddress]
+select * from sys_terminal where macaddress=$P{macaddress} 
+
+[unregisterTerminal] 
+update sys_terminal set 
+	macaddress=null, dtregistered=null, registeredby=null  
+where 
+	terminalid=$P{terminalid} 
+
 [list]
 select * from sys_terminal
 
@@ -15,6 +27,3 @@ select * from sys_terminal where terminalid = $P{terminalid}
 
 [find-clientid]
 select * from sys_terminal where terminalid=$P{terminal}
-
-[unregister-terminal]
-update sys_terminal set macaddress=null,dtregistered=null,registeredby=null  where terminalid=$P{terminalid}
