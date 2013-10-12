bca1ce093e366502a479c2ff775562a43e07bfd0
Jessie S. Zamora <jzamss@gmail.com>
2013-10-12 10:46:47 +0800

    Entity: add entityname to be indexed for searching. name is converted to text to accommodate very long names

--- workspace/services/shared/v25/modules/rameses-enterprise-entity-services.jar/sql/entityjuridical.sql
+++ workspace/services/shared/v25/modules/rameses-enterprise-entity-services.jar/sql/entityjuridical.sql
@@ -2,5 +2,5 @@
 SELECT e.*, ej.* 
 FROM entity e 
 	INNER JOIN entityjuridical ej ON e.objid=ej.objid 
-WHERE e.name LIKE $P{name} 
-ORDER BY e.name 
+WHERE e.entityname LIKE $P{name} 
+ORDER BY e.entityname 
